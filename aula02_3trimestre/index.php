e734aea11ae12c367f1ce9032bdbd68f8031e1dc
polianalinda <poliana.npereira2@aluno.edu.es.gov.br>
2025-09-03 11:01:43 -0300

    Create index.php

---
+++ aula02_3trimestre/index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <tile>Aula 02 PHP</title>
+    </head>
+    <body>
+        <?php
+            $nome="Polibanana";
+            echo "Meu nome é" .$nome;
+        ?>
+    </body>
+</html>
