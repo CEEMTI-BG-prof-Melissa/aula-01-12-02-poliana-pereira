c696761d99b49924bc274f3891a2d27cd31d0839
polianalinda <poliana.npereira2@aluno.edu.es.gov.br>
2025-09-10 10:49:22 -0300

    Update index.php

--- aulas3_3trimestre/index.php
+++ aulas3_3trimestre/index.php
@@ -12,13 +12,14 @@
             $soma= $n1+$n2;
 
             echo "Meu nome é: " .$nome1  ." e minha dupla é: " .$nome2;
-            echo "<br> Meu nome é: $nome1 e minha dupla é: $nome2.";
+            echo "<br> Meu nome é: $nome2 e minha dupla é: $nome1.";
 
             echo "<br> <br> A soma de $n1 com $n2 é igual a $soma.";
 
         ?>
     </body>
 </html>
+
 
 
 
