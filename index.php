093e1f1d532c1779bc240f65f340d02131774780
polianalinda <poliana.npereira2@aluno.edu.es.gov.br>
2025-09-10 10:29:37 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+    </head>
+    <body>
+        <?php
+
+            $nome1="Poliana";
+            $nome2="Polibanana";
+
+            $n1=10;
+            $n2=2;
+            $soma= $n1+$n2;
+
+            echo "Meu nome é: " .$nome1  ."e minha dupla é:" .$nome2;
+            echo "<br> Meu nome é: $nome1 e minha dupla é: $nome2.";
+
+            echo "<br> <br> A soma de $n1 com $n2 é igual a $soma."
+
+        ?>
+    </body>
+</html>
