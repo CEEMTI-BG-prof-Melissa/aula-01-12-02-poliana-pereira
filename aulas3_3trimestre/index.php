09d7746cf74d0faf9bf16c0da5fd95ac7f639bd2
polianalinda <poliana.npereira2@aluno.edu.es.gov.br>
2025-09-10 10:54:01 -0300

    Update index.php

--- aulas3_3trimestre/index.php
+++ aulas3_3trimestre/index.php
@@ -11,15 +11,19 @@
             $n2=2;
             $soma= $n1+$n2;
 
-            echo "Meu nome é: " .$nome1  ." e minha dupla é: " .$nome2;
+            echo "Meu nome é: " .$nome1  ." e minha dupla é: " .$nome2 ;
             echo "<br> Meu nome é: $nome2 e minha dupla é: $nome1.";
 
             echo "<br> <br> A soma de $n1 com $n2 é igual a $soma.";
 
+            echo "<br> <br> O resultado da subtração de $n1 e $n2 é igual a: " .($n1-$n2) .".";
+
+            echo "<br> <br> A multiplicação do $n1 e $n2 é igual a: " .($n1*$n2) .".";
+
+            echo "<br> <br> O resultado da divisão do $n1 e $n2 é igual a: " .($n1/$n2) .".";
+
+            echo "<br> <br> O resto do resultado da divisão de $n1 e $n2 é igual a: " .($n1%$n2) .".";
+
         ?>
     </body>
 </html>
-
-
-
-
